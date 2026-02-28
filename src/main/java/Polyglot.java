8dca23ec6154f61315e168b285a61f44ab47a2cf
Denisa <denisadamian2005@gmail.com>
2026-02-28 18:18:01 +0200

    schimbare 1

--- src/main/java/Polyglot.java
+++ src/main/java/Polyglot.java
@@ -3,18 +3,13 @@ import org.graalvm.polyglot.*;
 //clasa principala - aplicatie JAVA
 class Polyglot {
     //metoda privata pentru conversie low-case -> up-case folosind functia toupper() din R
-    private static String RToUpper(String token){
-        //construim un context care ne permite sa folosim elemente din R
-        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        //folosim o variabila generica care va captura rezultatul excutiei funcitiei R, toupper(String)
-        //pentru aexecuta instructiunea I din limbajul X, folosim functia graalvm polyglot.eval("X", "I");
-        Value result = polyglot.eval("R", "toupper(\""+token+"\");");
-        //utilizam metoda asString() din variabila incarcata cu output-ul executiei pentru a mapa valoarea generica la un String
-        String resultString = result.asString();
-        // inchidem contextul Polyglot
-        polyglot.close();
-
-        return resultString;
+    private static String PythonToUpper(String token) {
+        // Construim un context care permite accesul la Python
+        try (Context polyglot = Context.newBuilder().allowAllAccess(true).build()) {
+            // În Python, metoda .upper() transformă textul în majuscule
+            Value result = polyglot.eval("python", "'" + token + "'.upper()");
+            return result.asString();
+        }
     }
 
     //metoda privata pentru evaluarea unei sume de control simple a literelor unui text ASCII, folosind PYTHON
@@ -34,14 +29,15 @@ class Polyglot {
 
     //functia MAIN
     public static void main(String[] args) {
+        System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
         //construim un context pentru evaluare elemente JS
         Context polyglot = Context.create();
         //construim un array de string-uri, folosind cuvinte din pagina web:  https://chrisseaton.com/truffleruby/tenthings/
-        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\",\"command\",\"included\",\"in\",\"GraalVM\",\"we\",\"will\",\"be\",\"automatically\",\"using\",\"the\",\"Graal\",\"JIT\",\"compiler\",\"no\",\"extra\",\"configuration\",\"is\",\"needed\",\"I\",\"will\",\"use\",\"the\",\"time\",\"command\",\"to\",\"get\",\"the\",\"real\",\"wall\",\"clock\",\"elapsed\",\"time\",\"it\",\"takes\",\"to\",\"run\",\"the\",\"entire\",\"program\",\"from\",\"start\",\"to\",\"finish\",\"rather\",\"than\",\"setting\",\"up\",\"a\",\"complicated\",\"micro\",\"benchmark\",\"and\",\"I\",\"will\",\"use\",\"a\",\"large\",\"input\",\"so\",\"that\",\"we\",\"arent\",\"quibbling\",\"about\",\"a\",\"few\",\"seconds\",\"here\",\"or\",\"there\",\"The\",\"large.txt\",\"file\",\"is\",\"150\",\"MB\"];");
+        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\"];");
         //pentru fiecare cuvant, convertim la upcase folosind R si calculam suma de control folosind PYTHON
         for (int i = 0; i < array.getArraySize();i++){
             String element = array.getArrayElement(i).asString();
-            String upper = RToUpper(element);
+            String upper = PythonToUpper(element);
             int crc = SumCRC(upper);
             System.out.println(upper + " -> " + crc);
         }
